04a0299fb199f41e8ae8e4869fb6dcbc45e46c71
Santipap Patummanon <santipap.p@innovaitonssolution.com>
2025-07-06 15:16:36 +0000

    Refactor Swagger configuration to always enable OpenAPI and Swagger UI

--- Program.cs
+++ Program.cs
@@ -50,12 +50,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
